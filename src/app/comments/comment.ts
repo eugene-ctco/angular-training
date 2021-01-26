625d8691218d5a0bd6a69f2404774a8ac749e3de
Eugene Mihailishen <Eugene.Mihailishen@genre.com>
2021-01-26 14:34:40 +0200

    Auto-generated commit

--- src/app/comments/comment.ts
+++
@@ -1,7 +0,0 @@
-export interface UserComment {
-    id: number;
-    postId: number;
-    name: string;
-    email: string;
-    body: string;
-}
--- src/app/comments/state/username.actions.ts
+++
@@ -1,8 +0,0 @@
-import { createAction, props } from '@ngrx/store';
-import { UserComment } from '../comment';
-
-export const commentSubmit = createAction('[Comment] Comment submitted',
-    props<{ comment: UserComment }>());
-
-export const rememberUsername = createAction('[Comment] Save username',
-    props<{ username: string }>());
